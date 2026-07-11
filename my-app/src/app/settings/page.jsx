dc528446f7609fc49f01a47e997104e35fcbb94b
suyogm32 <suyogm32@gmail.com>
2026-07-12 00:45:51 +0530

    added github actions and fixed code structure

--- my-app/src/app/settings/page.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Applayout from '@/app/component/Applayout'
-const Settings = () => {
-  return (
-    <Applayout>This is settings page.</Applayout>
-  )
-}
-
-export default Settings
